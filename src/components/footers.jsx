4dc01e592a8f7071f6d6971e16863af6d61ab25c
ardazh <127732352+ardazh@users.noreply.github.com>
2023-06-10 21:37:54 +0700

    create: components header & footer

---
+++ src/components/footers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footers() {
+  return (
+    <footer className='flex justify-between px-[150px] py-[20px] bg-primary text-[#EFEFEFE5]'>
+        <div>2023 CashFlow. All right reserved.</div>
+        <div className='flex font-semibold'>
+            <div>+62 5637 8882 9901</div>
+            <div>contact@cashflow.com</div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footers
