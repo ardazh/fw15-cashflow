ff790e41b688095ccf3f898bbefce9352425f429
ardazh <rifaardan@gmail.com>
2023-08-07 17:04:04 +0700

    feat: add reducers profile & transactions

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth";
+import profile from "./profile";
+import transactions from "./transactions";
 
 const reducer = combineReducers({
   auth,
+  profile,
+  transactions,
 });
 
 export default reducer;
