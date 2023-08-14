f379d03fd7f9eb5c5822a55be616a02d0fa10820
ardazh <rifaardan@gmail.com>
2023-08-15 00:14:20 +0700

    fix: implementations Search, sort, pagination

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -4,13 +4,99 @@ import Footers from "@/components/footers";
 import Aside from "@/components/Aside";
 
 import Image from "next/image";
-import profile5 from "../assets/profile5.png";
+import defaultPict from "../assets/profile1.png";
+import { withIronSessionSsr } from "iron-session/next";
+import checkCredentials from "@/helpers/checkCredentials";
+import cookieConfig from "@/helpers/cookieConfig";
+import { useSelector } from "react-redux";
+import http from "@/helpers/http";
+import {
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { FaFilter } from "react-icons/fa";
 
-function History() {
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps({ req, res }) {
+    const token = req.session?.token;
+    checkCredentials(token, res, "/auth/login");
+    return {
+      props: {
+        token,
+      },
+    };
+  },
+  cookieConfig
+);
+
+function History({ token }) {
+  const profile = useSelector((state) => state.profile.data);
+  const [filter, setFilter] = React.useState("all");
+  const [search, setSearch] = React.useState("");
+  const [history, setHistory] = React.useState([]);
+  // const [page, setPage] = React.useState({});
+  const [sortList, setSortList] = React.useState("ASC");
+
+  const toggleSortOrder = () => {
+    const newSortOrder = sortList === "asc" ? "desc" : "asc";
+    setSortList(newSortOrder);
+
+    const sorted = history.results.slice().sort((a, b) => {
+      const fullNameA = (a.recipient?.fullName || "").toLowerCase();
+      const fullNameB = (b.recipient?.fullName || "").toLowerCase();
+
+      if (newSortOrder === "asc") {
+        return fullNameA.localeCompare(fullNameB);
+      } else {
+        return fullNameB.localeCompare(fullNameA);
+      }
+    });
+
+    setHistory({ ...history, results: sorted });
+  };
+
+  const getUsersTransaction = React.useCallback(
+    async (page = 1, searchTerm = "", filter = "all", type = "all") => {
+      try {
+        const { data } = await http(token).get("/transactions", {
+          params: {
+            page,
+            search: searchTerm,
+            filter,
+            type,
+          },
+        });
+        setHistory(data);
+        setFilter(data);
+      } catch (error) {
+        console.error("Error data transactions:", error);
+      }
+    },
+    [token]
+  );
+
+  React.useEffect(() => {
+    getUsersTransaction();
+  }, [getUsersTransaction]);
+
+  React.useEffect(() => {
+    getUsersTransaction(1, search);
+  }, [search, getUsersTransaction]);
+
+  const handleSort = (value) => {
+    setFilter(value);
+    if (value === "income") {
+      getTransaction(1, search, "all", "TOP-UP");
+    } else {
+      getTransaction(1, search, value);
+    }
+  };
+
   return (
     <main className="bg-[#E5E5E5]">
       <div>
-        <Headers />
+        <Headers token={token} />
       </div>
       <div className="flex">
         <div>
@@ -21,96 +107,231 @@ function History() {
             <div className="font-bold text-lg text-[#3A3D42]">
               Transaction History
             </div>
-            <div className="font-semibold text-sm text-primary">
-              <button className="btn btn-info text-black normal-case">
-                -- Select FIlter --
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <div className="h-14 w-full flex items-center gap-2 border-2 rounded-xl">
+              <i className="pl-5">
+                <AiOutlineSearch size={24} />
+              </i>
+              <div>
+                <input
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="text"
+                  name="searchName"
+                  className="outline-none w-full"
+                  placeholder="Search receiver here"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                onClick={toggleSortOrder}
+                className="btn w-50% btn-secondary capitalize text-white"
+              >
+                Sort {sortList === "asc" ? "Descending" : "Ascending"}
               </button>
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
-                </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
+          <div className="flex flex-col gap-5">
+            <div className="flex justify-between font-bold">
+              Search for: {search}
             </div>
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
+            <div>
+              {history.results && (
+                <div className="flex flex-col gap-4">
+                  {history.results.length === 0 ? (
+                    <p className="text-[#7a7878]">You have`t data yet</p>
+                  ) : (
+                    history.results
+                      .filter((item) => {
+                        const fullName = (
+                          (item.recipient && item.recipient.fullName) ||
+                          ""
+                        ).toLowerCase();
+                        return fullName.includes(search.toLowerCase());
+                      })
+                      .map((item) => {
+                        return (
+                          <div
+                            className="flex justify-between"
+                            key={`history-list-${item?.id}`}
+                          >
+                            {item.type === "TOP-UP" && (
+                              <div className="flex items-center justify-start gap-2">
+                                <div className="">
+                                  {item.recipient.picture ? (
+                                    <Image
+                                      width={150}
+                                      height={150}
+                                      className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                      src={item.recipient.picture}
+                                      alt="userImage"
+                                    />
+                                  ) : (
+                                    <Image
+                                      className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                      src={defaultPict}
+                                      alt="user"
+                                    />
+                                  )}
+                                </div>
+                                <div className="">
+                                  <div
+                                    className={`text-neutral text-base font-semibold ${
+                                      item?.recipient?.fullName
+                                        ? "capitalize"
+                                        : ""
+                                    }`}
+                                  >
+                                    {item?.recipient?.fullName ||
+                                      item?.recipient?.email}
+                                  </div>
+                                  <div className="text-">Topup</div>
+                                </div>
+                              </div>
+                            )}
+
+                            {item.type === "TRANSFER" && (
+                              <>
+                                {item.recipient.id !== profile.id && (
+                                  <div className="flex items-center justify-start gap-3">
+                                    <div className="">
+                                      {item.recipient.picture ? (
+                                        <Image
+                                          width={150}
+                                          height={150}
+                                          className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                          src={item.recipient.picture}
+                                          alt="userImage"
+                                        />
+                                      ) : (
+                                        <Image
+                                          className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                          src={defaultPict}
+                                          alt="user"
+                                        />
+                                      )}
+                                    </div>
+                                    <div className="">
+                                      <div
+                                        className={`text-neutral text-base font-semibold ${
+                                          item?.recipient?.fullName
+                                            ? "capitalize"
+                                            : ""
+                                        }`}
+                                      >
+                                        {item?.recipient?.fullName ||
+                                          item?.recipient?.email}
+                                      </div>
+                                      <div className="text-">Outcome</div>
+                                    </div>
+                                  </div>
+                                )}
+
+                                {item.recipient.id === profile.id && (
+                                  <div className="flex items-center justify-start gap-3">
+                                    <div className="">
+                                      {item.recipient.picture ? (
+                                        <Image
+                                          width={150}
+                                          height={150}
+                                          className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                          src={item.recipient.picture}
+                                          alt="userImage"
+                                        />
+                                      ) : (
+                                        <Image
+                                          className="object-cover w-10 h-10 rounded-xl overflow-hidden"
+                                          src={defaultPict}
+                                          alt="user"
+                                        />
+                                      )}
+                                    </div>
+                                    <div className="">
+                                      <div
+                                        className={`text-neutral text-base font-semibold ${
+                                          item?.recipient?.fullName
+                                            ? "capitalize"
+                                            : ""
+                                        }`}
+                                      >
+                                        {item?.recipient?.fullName ||
+                                          item?.recipient?.email}
+                                      </div>
+                                      <div className="text-">Income</div>
+                                    </div>
+                                  </div>
+                                )}
+                              </>
+                            )}
+
+                            {item?.type === "TOP-UP" && (
+                              <div className="text-base font-semibold text-blue-500">
+                                +
+                                {item.amount &&
+                                  `Rp${Number(item.amount).toLocaleString(
+                                    "id"
+                                  )}`}
+                              </div>
+                            )}
+                            {item?.type === "TRANSFER" &&
+                              (item.sender.id !== profile.id ? (
+                                <div className="text-base font-semibold text-green-500">
+                                  +
+                                  {item.amount &&
+                                    `Rp${Number(item.amount).toLocaleString(
+                                      "id"
+                                    )}`}
+                                </div>
+                              ) : (
+                                <div className="text-base font-semibold text-primary">
+                                  -
+                                  {item.amount &&
+                                    `Rp${Number(item.amount).toLocaleString(
+                                      "id"
+                                    )}`}
+                                </div>
+                              ))}
+                          </div>
+                        );
+                      })
+                  )}
+                  <div className="flex gap-2 justify-center items-center">
+                    <button
+                      onClick={() =>
+                        getUsersTransaction(
+                          history.pageInfo.page - 1,
+                          search,
+                          filter
+                        )
+                      }
+                      disabled={history.pageInfo.page <= 1}
+                      className="btn btn-primary"
+                    >
+                      <AiOutlineArrowLeft />
+                    </button>
+                    <div className="font-bold">
+                      page {history.pageInfo.page} of{" "}
+                      {history.pageInfo.totalPage}
+                    </div>
+                    <button
+                      onClick={() =>
+                        getUsersTransaction(
+                          history.pageInfo.page + 1,
+                          search,
+                          filter
+                        )
+                      }
+                      disabled={
+                        history.pageInfo.page >= history.pageInfo.totalPage
+                      }
+                      className="btn btn-primary"
+                    >
+                      <AiOutlineArrowRight />
+                    </button>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
-                </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
-                </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
-                </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex">
-                <div className="mr-[15px]">
-                  <Image src={profile5} alt="profile5.png" />
-                </div>
-                <div className="flex flex-col gap-[9px] mr-[59px]">
-                  <div className="font-bold text-[#4D4B57]">Samuel Suhi</div>
-                  <div className="text-sm text-[#7A7886]">Accept</div>
-                </div>
-              </div>
-              <div className="flex items-center text-bold text-[#1EC15F]">
-                +Rp50.000
-              </div>
+              )}
             </div>
           </div>
         </div>
